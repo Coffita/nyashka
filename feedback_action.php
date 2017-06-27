bf8c9fcff74af4d0b1bb40256a3cebac28aa434d
Diana <coffita@mail.ru>
2017-06-27 14:49:23 +0300

    feedback style

--- feedback_action.php
+++ feedback_action.php
@@ -1,16 +1,20 @@
 <?php include 'header.php'; ?>
 
-<div class="feedback_wrapper">
-    <?php if (isset($_GET['error'])) : ?>
-        <p class="wrong_email">Wrong e-mail adress!</p>
-    <?php endif; ?>
+<div class="feedback_wrapper-1">
+    <div class="feedback_wrapper-2">
+        <div class="feedback_wrapper">
+            <?php if (isset($_GET['error'])) : ?>
+                <p class="wrong_email">Wrong e-mail adress!</p>
+            <?php endif; ?>
 
-    <form class="feedback" action="feedback.php" method="post">
-         <input required class="feedback_field" type="text" name="username" placeholder="Enter your name"><br>
-         <input required class="feedback_field" type="text" name="usermail" placeholder="Enter your e-mail"><br>
-         <textarea required class="feedback_field" name="message" rows="10" cols="50" placeholder="Enter your message"></textarea><br>
-         <button type="submit">Send</button>
-     </form>
+            <form class="feedback" action="feedback.php" method="post">
+                 <input required class="feedback_field username_field"  type="text" name="username" placeholder="Enter your name"><br>
+                 <input required class="feedback_field usermail_field" type="text" name="usermail" placeholder="Enter your e-mail"><br>
+                 <textarea required class="feedback_field message_field" name="message" rows="10" cols="50" placeholder="Enter your message"></textarea><br>
+                 <button type="submit" class="feedback_field button">Send</button>
+             </form>
+        </div>
+    </div>
 </div>
 
 <?php include 'footer.php'; ?>
