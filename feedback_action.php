6572bff27f7cd073e345f486464fef308c4460bb
Diana <coffita@mail.ru>
2017-06-28 04:04:53 +0300

    made changes to feedback style

--- feedback_action.php
+++ feedback_action.php
@@ -3,11 +3,11 @@
 <div class="feedback_wrapper-1">
     <div class="feedback_wrapper-2">
         <div class="feedback_wrapper">
-            <?php if (isset($_GET['error'])) : ?>
-                <p class="wrong_email">Wrong e-mail adress!</p>
-            <?php endif; ?>
-
             <form class="feedback" action="feedback.php" method="post">
+                <?php if (isset($_GET['error'])) : ?>
+                    <p class="wrong_email">Wrong e-mail adress!</p>
+                <?php endif; ?>
+                
                  <input required class="feedback_field username_field"  type="text" name="username" placeholder="Enter your name"><br>
                  <input required class="feedback_field usermail_field" type="text" name="usermail" placeholder="Enter your e-mail"><br>
                  <textarea required class="feedback_field message_field" name="message" rows="10" cols="50" placeholder="Enter your message"></textarea><br>
