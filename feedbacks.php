5d6c32fced4eb0ae006be61e82054fb161333df6
Diana <coffita@mail.ru>
2017-07-01 05:15:09 +0300

    made the style of feedbacks messages

--- feedbacks.php
+++ feedbacks.php
@@ -12,10 +12,19 @@ $feedbacks = array_reverse(Feedback::find_all());
         <div class="feedbacks">
             <?php foreach ($feedbacks as $feedback) : ?>
                 <article class="feedback">
-                    <h1 class="feedback_head">From: <?= $feedback->username ?></h1>
-                    <h1 class="feedback_mail">Mail: <?= $feedback->usermail ?></h1>
+                    <h1 class="feedback_header feedback_head">From: <?= $feedback->username ?></h1>
+                    <h1 class="feedback_header feedback_mail">Mail: <?= $feedback->usermail ?></h1>
                     <p class="feedback_message">Message: <?= $feedback->message ?></p>
-                    <p class="date_of_sending">Message sent: <?= date('j F, Y G:i', strtotime(date($feedback->time_of_sending))) ?></p>
+                    <div class="controls_of_feedback">
+                        <p class="feedback_added_inf">Message sent: <?= date('j F, Y G:i', strtotime(date($feedback->time_of_sending))) ?></p>
+                        <div class="buttons_of_feedback-wrapper">
+                            <div class="buttons_of_feedback">
+                                <a class="reply button_of_feedback" href="reply.php?id=<?=$post->id?>">Reply</a>
+                                <a class="button_of_feedback delete_feedback" href="delete_feedback.php?id=<?=$post->id?>">Delete</a>
+                            </div>
+                            <div class="end_of_feedback_message"></div>
+                        </div>
+                    </div>
                 </article>
             <?php endforeach; ?>
         </div>
