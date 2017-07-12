b94e3569ad7917302ae5c0b278a77ebfd2363b3b
Diana <coffita@mail.ru>
2017-07-13 02:10:38 +0300

    Some changes

--- onlyforme.php
+++ onlyforme.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/onlyforme.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
     </head>
 
     <body>
@@ -15,9 +16,9 @@
             <div class="onlyforme-wrapper-1">
                 <div class="onlyforme-wrapper">
                     <form class="onlyforme" action="onlyforme_login.php" method="post">
-                        <input class="onlyforme_field onlyforme_email_field" type="text" name="email" value="" placeholder="Введите e-mail"><br>
-                        <input class="onlyforme_field onlyforme_password_field" type="password" name="password" value="" placeholder="Введите пароль"><br>
-                        <button type="submit">Войти</button>
+                        <input class="onlyforme_field onlyforme_email_field" type="text" name="email" value="" placeholder="Enter your e-mail"><br>
+                        <input class="onlyforme_field onlyforme_password_field" type="password" name="password" value="" placeholder="Enter your password"><br>
+                        <button type="submit">Login</button>
                     </form>
                 </div>
             </div>
