f40708fe79e9d7f0e1f600791072e813f22ce3c9
Diana <coffita@mail.ru>
2017-07-12 12:50:51 +0300

    Added style for the page onlyforme.php

--- onlyforme.php
+++ onlyforme.php
@@ -11,11 +11,17 @@
 
         </header>
 
-        <form action="onlyforme_login.php" method="post">
-            <input type="text" name="email" value="" placeholder="Введите e-mail"><br>
-            <input type="password" name="password" value="" placeholder="Введите пароль"><br>
-            <button type="submit">Войти</button> 
-        </form>
+        <div class="onlyforme-wrapper-2">
+            <div class="onlyforme-wrapper-1">
+                <div class="onlyforme-wrapper">
+                    <form class="onlyforme" action="onlyforme_login.php" method="post">
+                        <input class="onlyforme_field onlyforme_email_field" type="text" name="email" value="" placeholder="Введите e-mail"><br>
+                        <input class="onlyforme_field onlyforme_password_field" type="password" name="password" value="" placeholder="Введите пароль"><br>
+                        <button type="submit">Войти</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 
 </html>
--- reply.php
+++ reply.php
@@ -8,11 +8,11 @@ require_once __DIR__ . '/models/Feedback.php';
     <div class="reply_wrapper-1">
         <div class="reply_wrapper">
             <article class="reply">
-                <p class="reply_field reply_usermail">To: <?= $feedback->usermail ?></p>
-                <p class="reply_field reply_username">Username: <?= $feedback->username ?></p>
+                <p class="reply_field reply_username username_field">Username: <?= $feedback->username ?></p>
+                <p class="reply_field reply_usermail usermail_field">To: <?= $feedback->usermail ?></p>
                 <p class="reply_field reply_topic">Subject of topic: <?= $feedback->topic ?></p>
                 <form action="reply_form.php?id=<?=$feedback->id?>" method="post">
-                    <textarea class="reply_field reply_message" name="message_reply" rows="10" cols="50"></textarea><br>
+                    <textarea class="reply_field reply_message message_field" name="message_reply" rows="10" cols="50"></textarea><br>
                     <button class="reply_field button" type="button" name="button">Reply</button>
                 </form>
             </article>
