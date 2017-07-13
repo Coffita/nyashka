7dbf4c489057ba85e6c98a787cfbeda2444e5600
Diana <coffita@mail.ru>
2017-07-13 03:49:19 +0300

    Added style for new posts

--- new_post.php
+++ new_post.php
@@ -1,11 +1,15 @@
 <?php include 'header.php'; ?>
 
-<form action="create_post.php" method="post">
-    <div class="new_post1">
-        <input class="new_post" type="text" name="title" value=""></br>
-        <textarea class="new_post" rows="10" cols="50" wrap="soft | hard" name="text"></textarea></br>
-        <button type="submit">Добавить</button>
+<div class="new_post-wrapper-2">
+    <div class="new_post-wrapper-1">
+        <div class="new_post-wrapper">
+            <form class="new_post" action="create_post.php" method="post">
+                <input class="new_post_field new_post_title_field" type="text" name="title" placeholder="Enter your title"></br>
+                <textarea class="new_post_field new_post_text_field" rows="10" cols="50" wrap="soft | hard" name="text" placeholder="Enter your text"></textarea></br>
+                <button class="button new_post_field" type="submit">Add</button>
+            </form>
+        </div>
     </div>
-</form>
+</div>
 
 <?php include 'footer.php'; ?>
